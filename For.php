071289095d877060f5b4e67f600960dfdaa06257
RMrsa27 <69501671+RMrsa27@users.noreply.github.com>
2020-08-18 17:05:39 +0700

    Create For.php

---
+++ For.php
@@ -0,0 +1,30 @@
+<?php
+for ($i= 1; $i <= 10; $i++)
+{
+   echo "Saya sedang belajar PHP <br/>";
+}
+
+ echo "<br/>";
+
+for ($i= 1; $i <= 5; $i++)
+{
+   echo $i;
+   echo "<br/>";
+}
+
+echo "<br/>";
+
+for ($i= 0; $i <= 60; $i=$i+5)
+{
+   echo $i;
+   echo "<br/>";
+}
+
+echo "<br/>";
+
+for ($i= 10; $i >= 1; $i--)
+{
+   echo $i;
+   echo "<br/>";
+}
+?>
